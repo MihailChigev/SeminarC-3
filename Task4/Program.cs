2466d1210a263f48013914208b83ce06daa0831b
MikeChigev <mchigev@gmail.com>
2022-12-01 18:27:13 +0300

    Напишите программу которая принимант число N и выдаеттаблицу чисел от 1 до N

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу которая принимант число N и выдаеттаблицу чисел от 1 до N
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+Console.Clear();
+
+
+
+
+
+void TablePOW2(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    int count = 1;
+    while (count <= number)
+    {
+        int pow = count*count;
+        Console.Write(count*count + " ");
+        count++;
+    }
+    Console.WriteLine();
+
+}
+
+TablePOW2("Введите число N: ");
